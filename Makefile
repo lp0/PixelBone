7bfe5a19aef32e6249ca55abe198a066fc4a189d
Katherine Whitlock <toroidalcode@gmail.com>
2014-05-07 22:01:50 +0000

    Reflektor demo done

--- Makefile
+++ Makefile
@@ -41,7 +41,7 @@ CXXFLAGS += \
 	-Wp,-MMD,$(dir $@).$(notdir $@).d \
 	-Wp,-MT,$@ \
 	-I. \
-	-O2 \
+	-O3 \
 	-mtune=cortex-a8 \
 	-march=armv7-a \
 
@@ -92,7 +92,7 @@ $(TARGETS):
 	$(CXX) $(CXXFLAGS) $(LDFLAGS) -o $@ $^ $(LDLIBS)
 
 examples/reflektor: examples/reflektor.cpp
-	$(CXX) $(CXXFLAGS) $(LDFLAGS) -o $@ $^ $(LDLIBS) $(PIXELBONE_OBJS) $(APP_LOADER_LIB) -lfftw3f -lm -lportaudio
+	$(CXX) $(CXXFLAGS) -g $(LDFLAGS) -o $@ $^ $(LDLIBS) $(PIXELBONE_OBJS) $(APP_LOADER_LIB) -lfftw3f -lm -lportaudio
 
 
 .PHONY: clean
--- examples/reflektor.cpp
+++ examples/reflektor.cpp
@@ -23,8 +23,6 @@
 **          Version: 1.2.1                                                **
 ****************************************************************************/
 
-#include <cstdlib>
-#include <cstdio>
 #include <cstring>
 #define _USE_MATH_DEFINES
 #include <cmath>
@@ -33,12 +31,12 @@
 
 #include <fftw3.h>
 #include <string>
-#include <iostream>
-#include <sys/time.h>
 
 #include <vector>
 #include <portaudio.h>
 #include <numeric>
+#include <thread>
+#include <mutex>
 #include "../matrix.hpp"
 #define NUM_BINS 6
 
@@ -57,76 +55,103 @@ using namespace std;
 double gInOutScaler = 1.0;
 #define CONVERT_IN_TO_OUT(in)  ((float) ((in) * gInOutScaler))
 
-static int gNumNoInputs = 0;
-/* This routine will be called by the PortAudio engine when audio is needed.
-** It may be called at interrupt level on some machines so don't do anything
-** that could mess up the system like calling malloc() or free().
-*/
 static fftwf_complex left_out[FRAMES_PER_BUFFER], right_out[FRAMES_PER_BUFFER];
 static fftwf_plan lp, rp;
+static float left_mid[FRAMES_PER_BUFFER], right_mid[FRAMES_PER_BUFFER];
+std::mutex plan_mtx;           // mutex for plans
+std::mutex mid_mtx;
+std::mutex out_mtx;
 
-static int fftwCallback(const void *inputBuffer, void *outputBuffer,
-                        unsigned long framesPerBuffer,
-                        const PaStreamCallbackTimeInfo *timeInfo,
-                        PaStreamCallbackFlags statusFlags, void *userData) {
+bool plan_set;                 // indicates if plans exist
+
+static PixelBone_Matrix *matrix;
+std::mutex matrix_mtx;
+
+inline static double average(vector<float> &v) {
+  double sum = std::accumulate(std::begin(v), std::end(v), 0.0);
+  return sum / v.size();
+}
+
+inline static float scale(float oldMin, float oldMax, float newMin, float newMax, float value){
+  return (((newMax - newMin) * (value - oldMin)) / (oldMax - oldMin)) + newMin;
+}
+
+
+static void fftwProcess(const void *inputBuffer) {
+  
+  // printf("FFTW CALLBACK GOT CALLED\n");
+  
   float **input_ptr_ary = (float **)inputBuffer;
   float *left_in = input_ptr_ary[0];
   float *right_in = input_ptr_ary[1];
 
-  if (lp == NULL && rp == NULL) {
-    lp = fftwf_plan_dft_r2c_1d(FRAMES_PER_BUFFER, left_in, left_out, FFTW_MEASURE);
-    rp = fftwf_plan_dft_r2c_1d(FRAMES_PER_BUFFER, right_in, right_out, FFTW_MEASURE);
-  }
+  // if (lp == NULL && rp == NULL ) {
+  //   plan_mtx.lock();
+  //   printf("FFTW Plans are null\n");
+  //   if (!plan_set) {
+  //     plan_set = true;
 
-  (void)timeInfo; /* Prevent unused variable warnings. */
-  (void)statusFlags;
-  (void)userData;
-  (void)outputBuffer;
+  //   }
+  //   plan_mtx.unlock();
+  // }
 
-  if (inputBuffer == NULL) {
-    gNumNoInputs += 1;
-  }
+  if (inputBuffer == NULL) return;
 
+  mid_mtx.lock();
   /* Hanning window function */
-  for (uint i = 0; i < framesPerBuffer; i++) {
-    double multiplier = 0.5 * (1 - cos(2 * M_PI * i / (framesPerBuffer - 1)));
-    left_in[i] = multiplier * (left_in[i] + 1.0);
-    right_in[i] = multiplier * (right_in[i] + 1.0);
+  for (uint i = 0; i < FRAMES_PER_BUFFER; i++) {
+    double multiplier = 0.5 * (1 - cos(2 * M_PI * i / (FRAMES_PER_BUFFER - 1)));
+    left_mid[i] = multiplier * (left_in[i] + 1.0);
+    right_mid[i] = multiplier * (right_in[i] + 1.0);
   }
+  mid_mtx.unlock();
 
+  out_mtx.lock();
   fftwf_execute(lp);
   fftwf_execute(rp);
+  out_mtx.unlock();
 
-  float *in;
-  float *out;
-  int inDone = 0;
-  int outDone = 0;
-  unsigned int i;
-  int inChannel, outChannel;
-  
-  /* This may get called with NULL inputBuffer during initial setup. */
-  if( inputBuffer == NULL) return 0;
-  
-  inChannel=0, outChannel=0;
-  while( !(inDone && outDone) ) {
-    in = ((float**)inputBuffer)[inChannel];
-    out = ((float**)outputBuffer)[outChannel];
-    
-    for( i=0; i<framesPerBuffer; i++ ) {
-      *out = CONVERT_IN_TO_OUT( *in );
-      out += 1;
-      in += 1;
-    }
-    
-    if(inChannel < (2 - 1)) inChannel++;
-    else inDone = 1;
-    if(outChannel < (2 - 1)) outChannel++;
-    else outDone = 1;
+  // Add data:
+  vector<float> tempdata;
+  for (uint i = 2; i < (FRAMES_PER_BUFFER / 8) - 1; i++) {
+    //ticks << (i * SAMPLE_RATE / FRAMES_PER_BUFFER);
+    //           right_out[i][0]);
+     tempdata.push_back(abs(left_out[i][0]));
   }
+  
+  matrix_mtx.lock();
+  matrix->clear();
+  uint x =0;
+  for (auto it = tempdata.begin(); !(it >= tempdata.end()); it+=4) { 
+    vector<float> temp(it,it+4); 
+     int datum = (int) floor(scale(0, 30, 0, 8, average(temp)));
+     matrix->drawFastVLine(x++, 9, -datum, PixelBone_Pixel::Color(150,150,150));
+  }  
+  matrix->wait();
+  matrix->show();
+  //matrix->moveToNextBuffer();
+  matrix_mtx.unlock();
+}
+
+/** 
+ * This routine will be called by the PortAudio engine when audio is needed.
+ * It may be called at interrupt level on some machines so don't do anything
+ * that could mess up the system like calling malloc() or free().
+ */
+static int copyCallback(const void *inputBuffer, void *outputBuffer,
+                        unsigned long framesPerBuffer,
+                        const PaStreamCallbackTimeInfo *timeInfo,
+                        PaStreamCallbackFlags statusFlags, void *userData) {
+  //    Copy stuff
+  if( inputBuffer == NULL) return 0;
+  memcpy(((float**)outputBuffer)[0], ((float**)inputBuffer)[0], framesPerBuffer * sizeof(float));
+  memcpy(((float**)outputBuffer)[1], ((float**)inputBuffer)[1], framesPerBuffer * sizeof(float));
+  std::thread (fftwProcess, inputBuffer).detach();
   return paContinue;
 }
 
-void setupAudio(PaStream *stream) {
+
+void setupAudio(PaStream *stream, PaStreamCallback *streamCallback) {
   PaStreamParameters inputParameters, outputParameters;
   PaError err;
 
@@ -155,66 +180,46 @@ void setupAudio(PaStream *stream) {
       Pa_GetDeviceInfo(outputParameters.device)->defaultLowInputLatency;
   outputParameters.hostApiSpecificStreamInfo = NULL;
 
+  // PaAlsa_EnableRealtimeScheduling(stream, true);
 
   err = Pa_OpenStream(&stream, &inputParameters, &outputParameters, SAMPLE_RATE,
                       FRAMES_PER_BUFFER, 0,
                       /* paClipOff, */ /* we won't output out of range samples
                                           so don't bother clipping them */
-                      fftwCallback, NULL);
-  if (err != paNoError)
+                      streamCallback, NULL);
+  if (err != paNoError) {
+    printf("Error opening stream\n");
     Pa_Terminate();
+  }
 
   err = Pa_StartStream(stream);
-  if (err != paNoError)
+  if (err != paNoError) {
+    printf("Error starting stream\n");
     Pa_Terminate();
+  }
 }
 
 
 
 
-inline static double average(vector<float> &v) {
-  double sum = std::accumulate(std::begin(v), std::end(v), 0.0);
-  return sum / v.size();
-}
-
-inline static float scale(float oldMin, float oldMax, float newMin, float newMax, float value){
-  return (((newMax - newMin) * (value - oldMin)) / (oldMax - oldMin)) + newMin;
-}
 
 
 /*******************************************************************/
 int main(void) {
-  PixelBone_Matrix matrix(16,8,4,1,
-                          TILE_TOP   + TILE_LEFT   + TILE_ROWS   + TILE_PROGRESSIVE +
-                          MATRIX_TOP + MATRIX_LEFT + MATRIX_ROWS + MATRIX_ZIGZAG);
-
- PaStream *stream = nullptr;
-
- setupAudio(stream);
-
- while (true) {
-   matrix.clear();
-   // Add data:
-   vector<float> tempdata;
-   for (uint i = 2; i < (FRAMES_PER_BUFFER / 8) - 1; i++) {
-     //ticks << (i * SAMPLE_RATE / FRAMES_PER_BUFFER);
-     //           right_out[i][0]);
-     tempdata.push_back(abs(left_out[i][0]));
-   }
-   
-   uint x =0;
-   for (auto it = tempdata.begin(); !(it >= tempdata.end()); it+=4) { 
-     vector<float> temp(it,it+4); 
-     int datum = (int) floor(scale(0, 75, 0, 8, average(temp)));
-     matrix.drawFastVLine(x++, 8, -datum, PixelBone_Pixel::Color(150,150,150));
-   }  
-   matrix.wait();
-   matrix.show();
-   matrix.moveToNextBuffer();
- }
-
- Pa_CloseStream(stream);
- Pa_Terminate();
- std::cout << "Quiting!" << std::endl;
- return 0;
+  lp = fftwf_plan_dft_r2c_1d(FRAMES_PER_BUFFER, left_mid, left_out, FFTW_MEASURE);
+  rp = fftwf_plan_dft_r2c_1d(FRAMES_PER_BUFFER, right_mid, right_out, FFTW_MEASURE);
+  matrix = new PixelBone_Matrix(16,8,4,1,
+                        TILE_TOP   + TILE_LEFT   + TILE_ROWS   + TILE_PROGRESSIVE +
+                        MATRIX_TOP + MATRIX_LEFT + MATRIX_ROWS + MATRIX_ZIGZAG);
+  PaStream *stream = nullptr;
+  setupAudio(stream, copyCallback);
+  
+  while (true) {
+    ;
+  }
+  
+  Pa_CloseStream(stream);
+  Pa_Terminate();
+  printf("Quiting!\n");
+  return 0;
 }
